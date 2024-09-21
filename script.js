71da169e3fa1fcab3cbd2a9fd8a486fe385e2522
tmtrinidad <0trinidad6@gmail.com>
2024-09-21 15:57:00 -0700

    remove input error message and hide block

--- script.js
+++ script.js
@@ -2,32 +2,25 @@ import Day from "./day.js";
 
 function viewCalculation() {
   var totalDays = document.getElementById("days").value;
-  var T = document.getElementById("calculations");
-  var Z = document.getElementById("no_calculation");
+  var calculations = document.getElementById("calculations");
+  if(totalDays > 0) {
+    calculations.style.display = "block";
+  } else {
+    return;
+  }
 
   var day = new Day(Number(totalDays));
 
   document.getElementById("current_day").innerHTML = totalDays;
   document.getElementById("years").innerHTML = day.getYears();
-  document.getElementById("season_day").innerHTML =
-    day.getCurrentSeasonDayWithOrdinalIndicator();
+  document.getElementById("season_day").innerHTML = day.getCurrentSeasonDayWithOrdinalIndicator();
   var currentSeasonElements = document.getElementsByClassName("current_season");
   for (var i = 0; i < currentSeasonElements.length; i++) {
     currentSeasonElements[i].innerHTML = day.getCurrentSeason();
   }
-  document.getElementById("days_left_of_season").innerHTML =
-    day.getDaysLeftOfSeason();
+  document.getElementById("days_left_of_season").innerHTML = day.getDaysLeftOfSeason();
   document.getElementById("next_season").innerHTML = day.getNextSeason();
-  document.getElementById("day_of_next_season").innerHTML =
-    day.getDayOfNextSeason();
-
-  if (totalDays > 0) {
-    Z.style.display = "none";
-    T.style.display = "block";
-  } else {
-    T.style.display = "none";
-    Z.style.display = "block";
-  }
+  document.getElementById("day_of_next_season").innerHTML = day.getDayOfNextSeason();
 }
 
 function validateInput(event, input) {
