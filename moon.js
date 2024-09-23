f7ae002d4467575b07011c23ab6a7059143e1026
tmtrinidad <0trinidad6@gmail.com>
2024-09-22 18:01:01 -0700

    finished basic moon calculations

--- moon.js
+++ moon.js
@@ -1,17 +1,70 @@
 import Day from "./day.js";
+
 class Moon {
-    #MOON_PHASE_LENGTH = 20;
-    #NEW_MOON = "New Moon";
-    #QUARTER_MOON = "Quarter Moon";
-    #HALF_MOON = "Half Moon";
-    #WAXING_GIBBOUS_MOON = "3/4 (Waxing Gibbous) Moon";
-    #FULL_MOON = "Full Moon";
-    #WANING_GIBBOUS_MOON = "3/4 (Waning Gibbous) Moon";
+    #moonPhaseLength = 20;
+    #fullMoonDay = 11;
+    #newMoon = "New Moon";
+    #waxingCrescentMoon = "Quarter (Waxing Crescent) Moon";
+    #halfMoon = "Half Moon";
+    #waxingGibbousMoon = "3/4 (Waxing Gibbous) Moon";
+    #fullMoon = "Full Moon";
+    #waningGibbousMoon = "3/4 (Waning Gibbous) Moon";
+    #waningCrescentMoon = "Quarter (Waning Crescent) Moon";
+
+    #day;
+    #moonDay;
+    #moonPhase;
+    #daysUntilNextFullMoon;
     constructor(day) {
         if (!(day instanceof Day)) {
             throw new Error('Expected an instance of Day.');
         }
-        this.day = day;
-        this.moonDay = day.getDays();
+        this.#day = day.getDays();
+        this.#moonDay = this.#setMoonDay();
+        this.#moonPhase = this.#setMoonPhase();
+        this.#daysUntilNextFullMoon = this.#setDaysUntilNextFullMoon();
+    }
+
+    #setMoonDay(){
+        var moonDay = this.#day % this.#moonPhaseLength;
+        if(moonDay == 0){
+            moonDay = this.#moonPhaseLength;
+        }
+        return moonDay;
+    }
+
+    #setMoonPhase(){
+        var moonPhaseMap = {
+            1: this.#newMoon,
+            2: this.#waxingCrescentMoon, 3: this.#waxingCrescentMoon, 4: this.#waxingCrescentMoon,
+            5: this.#halfMoon, 6: this.#halfMoon, 7: this.#halfMoon,
+            8: this.#waxingGibbousMoon, 9: this.#waxingGibbousMoon, 10: this.#waxingGibbousMoon,
+            11: this.#fullMoon,
+            12: this.#waningGibbousMoon, 13: this.#waningGibbousMoon, 14: this.#waningGibbousMoon,
+            15: this.#halfMoon, 16: this.#halfMoon, 17: this.#halfMoon,
+            18: this.#waningCrescentMoon, 19: this.#waningCrescentMoon, 20: this.#waningCrescentMoon
+        };
+
+        return moonPhaseMap[this.#moonDay];
+    }
+
+    #setDaysUntilNextFullMoon(){
+        if(this.#moonDay < this.#fullMoonDay){
+            return this.#fullMoonDay - this.#moonDay;
+        } else if(this.#moonDay == this.#fullMoonDay){
+            return 0;
+        } else {
+            return this.#moonPhaseLength - this.#moonDay + this.#fullMoonDay;
+        }
+    }
+
+    getMoonPhase(){
+        return this.#moonPhase;
+    }
+
+    getDaysUntilNextFullMoon(){
+        return this.#daysUntilNextFullMoon;
     }
 }
+
+export default Moon;
